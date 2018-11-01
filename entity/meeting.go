7811f41bce5e1b3b04802ba005125eb9442baf64
gyakkun <xiaowufeixia@gmail.com>
2018-11-01 22:06:28 +0800

    Var name refine.

--- entity/meeting.go
+++ entity/meeting.go
@@ -4,8 +4,6 @@ package entity
 
 import (
  	"strings"
-	// "Date"
-	// "agenda-go-cli/loghelper"
 )
 type Meeting struct {
 	Sponsor string
@@ -13,38 +11,40 @@ type Meeting struct {
 	StartDate, EndDate Date
 	Title string
 }
-func (m_meeting Meeting) init(t_Sponsor string, t_Participators []string, t_StartDate Date, t_EndDate Date,t_Tittle string) {
-	m_meeting.Sponsor= t_Sponsor
-	m_meeting.SetParticipator(t_Participators)
-	m_meeting.StartDate.CopyDate(t_StartDate)
-	m_meeting.EndDate.CopyDate(t_EndDate)
-	m_meeting.Title= t_Tittle
+func (mMeeting Meeting) init(tSponsor string, tParticipators []string, tStartDate Date, tEndDate Date,tTitle string) {
+	mMeeting.Sponsor= tSponsor
+	mMeeting.SetParticipator(tParticipators)
+	mMeeting.StartDate.CopyDate(tStartDate)
+	mMeeting.EndDate.CopyDate(tEndDate)
+	mMeeting.Title= tTitle
 }
-func (m_meeting Meeting) CopyMeeting (t_meeting Meeting) {
-	m_meeting.Sponsor= t_meeting.Sponsor
-	m_meeting.SetParticipator(t_meeting.Participators)
-	m_meeting.StartDate.CopyDate(t_meeting.StartDate)
-	m_meeting.EndDate.CopyDate(t_meeting.EndDate)
-	m_meeting.Title= t_meeting.Title
+
+func (mMeeting Meeting) CopyMeeting (tMeeting Meeting) {
+	mMeeting.Sponsor= tMeeting.Sponsor
+	mMeeting.SetParticipator(tMeeting.Participators)
+	mMeeting.StartDate.CopyDate(tMeeting.StartDate)
+	mMeeting.EndDate.CopyDate(tMeeting.EndDate)
+	mMeeting.Title= tMeeting.Title
 }
-func (m_meeting Meeting) GetSponsor() string {
-	return m_meeting.Sponsor
+
+func (mMeeting Meeting) GetSponsor() string {
+	return mMeeting.Sponsor
 }
 
 /**
 * @brief set the sponsor of a meeting
 * @param  the new sponsor string
 */
-func (m_meeting Meeting) SetSponsor(t_sponsor string) {
-	m_meeting.Sponsor = t_sponsor
+func (mMeeting Meeting) SetSponsor(tSponsor string) {
+	mMeeting.Sponsor = tSponsor
 }
 
 /**
 * @brief  get the participator of a meeting
 * @return return a string indicate participator
 */
-func (m_meeting Meeting) GetParticipator() []string {
-    return m_meeting.Participators
+func (mMeeting Meeting) GetParticipator() []string {
+    return mMeeting.Participators
 }
 
 /**
@@ -52,10 +52,10 @@ func (m_meeting Meeting) GetParticipator() []string {
 *   @param the new participator string
 */
 
-func (m_meeting Meeting) SetParticipator(t_participators []string) {
-	var length= len(t_participators)
+func (mMeeting Meeting) SetParticipator(tParticipators []string) {
+	var length= len(tParticipators)
 	for i := 0; i < length; i++ {
-		m_meeting.Participators[i]= t_participators[i]
+		mMeeting.Participators[i]= tParticipators[i]
 	}
 }
 
@@ -63,48 +63,48 @@ func (m_meeting Meeting) SetParticipator(t_participators []string) {
 * @brief get the startDate of a meeting
 * @return return a string indicate startDate
 */
-func (m_meeting Meeting) GetStartDate() Date {
-	return m_meeting.StartDate
+func (mMeeting Meeting) GetStartDate() Date {
+	return mMeeting.StartDate
 }
 
 /**
 * @brief  set the startDate of a meeting
 * @param  the new startdate of a meeting
 */
-func (m_meeting Meeting) SetStartDate(t_startTime Date) {
-    m_meeting.StartDate.CopyDate(t_startTime)
+func (mMeeting Meeting) SetStartDate(tStartTime Date) {
+    mMeeting.StartDate.CopyDate(tStartTime)
 }
 
 /**
 * @brief get the endDate of a meeting
 * @return a date indicate the endDate
 */
-func (m_meeting Meeting) GetEndDate() Date {
-	return m_meeting.EndDate
+func (mMeeting Meeting) GetEndDate() Date {
+	return mMeeting.EndDate
 }
 
 /**
 * @brief  set the endDate of a meeting
 * @param  the new enddate of a meeting
 */
-func (m_meeting Meeting) SetEndDate(t_endTime Date) {
-	m_meeting.EndDate.CopyDate(t_endTime)
+func (mMeeting Meeting) SetEndDate(t_endTime Date) {
+	mMeeting.EndDate.CopyDate(t_endTime)
 }
 
 /**
 * @brief get the title of a meeting
 * @return a date title the endDate
 */
-func (m_meeting Meeting) GetTitle() string {
-	return m_meeting.Title
+func (mMeeting Meeting) GetTitle() string {
+	return mMeeting.Title
 }
 
 /**
 * @brief  set the title of a meeting
 * @param  the new title of a meeting
 */
-func (m_meeting Meeting) SetTitle(t_title string) {
-	m_meeting.Title = t_title
+func (mMeeting Meeting) SetTitle(t_title string) {
+	mMeeting.Title = t_title
 }
 
 /**
@@ -112,29 +112,29 @@ func (m_meeting Meeting) SetTitle(t_title string) {
 * @param t_username the source username
 * @return if the user take part in this meeting
 */
-func (m_meeting Meeting) IsParticipator(t_username string) bool {
+func (mMeeting Meeting) IsParticipator(t_username string) bool {
   var i int
-	for i= 0; i< len(m_meeting.Participators); i++ {
-		if strings.EqualFold(m_meeting.Participators[i], t_username)== true {
+	for i= 0; i< len(mMeeting.Participators); i++ {
+		if strings.EqualFold(mMeeting.Participators[i], t_username)== true {
 	    	return true
 		}
 	}
 	return false
 }
-func (m_meeting *Meeting) DeleteParticipator(t_username string) {
+func (mMeeting *Meeting) DeleteParticipator(t_username string) {
 	var i int
-	tl := len(m_meeting.Participators)
+	tl := len(mMeeting.Participators)
 	for i= 0; i< tl; i++ {
-		if strings.EqualFold(m_meeting.Participators[i], t_username)== true {
-	    	m_meeting.Participators = append(m_meeting.Participators[:i], m_meeting.Participators[i+1:]...)
+		if strings.EqualFold(mMeeting.Participators[i], t_username)== true {
+	    	mMeeting.Participators = append(mMeeting.Participators[:i], mMeeting.Participators[i+1:]...)
 			break
 		}
 	}
 }
-func (m_meeting *Meeting) AddParticipator(t_username string) bool {
-	if strings.EqualFold(m_meeting.Sponsor, t_username) || m_meeting.IsParticipator(t_username) {
+func (mMeeting *Meeting) AddParticipator(t_username string) bool {
+	if strings.EqualFold(mMeeting.Sponsor, t_username) || mMeeting.IsParticipator(t_username) {
 		return false
 	}
-	m_meeting.Participators = append(m_meeting.Participators,t_username)
+	mMeeting.Participators = append(mMeeting.Participators,t_username)
 	return true
 }
